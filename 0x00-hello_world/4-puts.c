8ea185fc253226184bbe40c5da865990568417bb
Islam Nazeer <islamnazeer7@gmail.com>
2023-02-16 14:37:43 -0800

    yaraab

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main -A program that prints aline with puts 
+*n
+*Return:0(success) 
+*/
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
