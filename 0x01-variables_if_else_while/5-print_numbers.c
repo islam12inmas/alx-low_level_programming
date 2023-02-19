ef03ecf2cbab976eb81458d53d311017834a51ed
Islam Nazeer <islamnazeer7@gmail.com>
2023-02-19 09:38:43 -0800

    n

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - prints the numbers from 0 to 9
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for(int i=0; i<10; i++)
+	       printf("%d", i);
+	printf("\n");
+	return(0);
+}
